8c3b929927a8c0ac2a99e0d1336f75e0a71ae731
Kesav-21 <cgkesav@gmail.com>
2023-11-19 19:32:31 +0530

    Cards,data defined

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
       <img className="card-img" src="/src/assets/1.webp" alt="1" />
       <div className="card-desc">
         <div className="card-title">
-          <h3>Short.Short.Short-Loop</h3>
-          <p>CarryGrow</p>
+          <h3>{props.title}</h3>
+          <p>{props.author}</p>
         </div>
         <div className="card-interaction">
             <p>
                 <img src="/src/assets/like.svg" alt="" />
-                30
+                {props.likesCount}
             </p>
             <p>
                 <img src="/src/assets/view.svg" alt="" />
-                130
+                {props.viewsCount}
             </p>
         </div>
       </div>
--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Card from './Card'
+import { data } from '../data/data'
 
 const Listings = () => {
   return (
     <div className='listing-container'>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      {
+        data.map(e=>{
+          return (
+            <Card title={e.title} author={e.author} likesCount={e.likesCount} viewsCount={e.viewsCount}/>
+          )
+        })
+      }
     </div>
   )
 }
